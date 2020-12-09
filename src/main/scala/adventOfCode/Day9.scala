46e76546a25463cf488488c18455085eb72c3628
Hjalmgunnarson <wtvankampen@gmail.com>
2020-12-09 21:11:11 +0100

    Day 9 part 2

--- src/main/scala/adventOfCode/Day9.scala
+++ src/main/scala/adventOfCode/Day9.scala
@@ -1,15 +1,30 @@
 package adventOfCode
 
+import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.io.Source
 
 object Day9 extends App {
   val lines = Source.fromResource("day9Input.txt").getLines.toList
   val numbers = lines.map(_.toLong)
-
+  val invalidNumbers = mutable.ListBuffer.empty[Long]
   val preambleLength = 25
 
   numbers.sliding(preambleLength + 1).foreach {
-    case preamble :+ element => if (!calculateOptions(preamble).contains(element)) println(element)
+    case preamble :+ element => if (!calculateOptions(preamble).contains(element)) invalidNumbers.addOne(element)
+  }
+  // Part 1
+  println(invalidNumbers.min)
+
+  // Part 2
+  val contiguousSet = findContiguousSet(numbers, 1, invalidNumbers.min)
+  println(contiguousSet.min + contiguousSet.max)
+
+  @tailrec
+  def findContiguousSet(candidates: List[Long], endOfSlice: Int, number: Long): List[Long] = {
+    if (endOfSlice >= candidates.length) findContiguousSet(candidates.tail, 1, number)
+    else if (candidates.slice(0, endOfSlice).sum == number) candidates.slice(0, endOfSlice)
+    else findContiguousSet(candidates, endOfSlice + 1, number)
   }
 
   def calculateOptions(numbers: List[Long]): List[Long] = for {
@@ -17,4 +32,3 @@ object Day9 extends App {
     numberB <- numbers
   } yield numberA + numberB
 }
-
