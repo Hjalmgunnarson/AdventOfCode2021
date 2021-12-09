d1e70049b9a0f979bcccfe489a686b4e140dd698
Hjalmgunnarson <wtvankampen@gmail.com>
2021-12-09 20:37:10 +0100

    Day 2 part 2

--- src/main/scala/adventOfCode/Day2.scala
+++ src/main/scala/adventOfCode/Day2.scala
@@ -7,8 +7,9 @@ object Day2 extends App {
   val test = List("forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2")
 
   val commands = Source.fromResource("day2Input.txt").getLines.toList
-  val position = move(commands, Tuple2(0,0))
-  println(position._1 * position._2)
+  val position = move(commands, Tuple2(0, 0))
+  println("Part 1 :" + position._1 * position._2)
+  println("Part 2 :" + move(commands, PositionAndAim(0, 0, 0)).answer)
 
   // Part 1
   @tailrec
@@ -17,7 +18,22 @@ object Day2 extends App {
       case Nil => position
       case command :: tail =>
         val movement = processCommand(command)
-        move( tail, Tuple2(position._1 + movement._1, position._2 + movement._2))
+        move(tail, Tuple2(position._1 + movement._1, position._2 + movement._2))
+    }
+  }
+
+  // Part 2
+  @tailrec
+  def move(commands: List[String], positionAndAim: PositionAndAim): PositionAndAim = {
+    commands match {
+      case Nil => positionAndAim
+      case command :: tail =>
+        val movement = processCommand(command)
+        move(tail, PositionAndAim(
+          positionAndAim.x + movement._1,
+          positionAndAim.y + positionAndAim.aim * movement._1,
+          positionAndAim.aim + movement._2)
+        )
     }
   }
 
@@ -29,5 +45,11 @@ object Day2 extends App {
       case "up" => Tuple2(0, 0 - i.toInt)
       case "down" => Tuple2(0, i.toInt)
     }
+  }
+
+  case class PositionAndAim(x: Int, y: Int, aim: Int) {
+    override def toString: String = x + " " + y + " " + aim
+
+    def answer: Int = x * y
   }
 }
