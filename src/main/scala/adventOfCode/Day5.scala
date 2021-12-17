35e76e12cdbf458561c6dd9d801b1af9dc15fdc9
Hjalmgunnarson <wtvankampen@gmail.com>
2021-12-17 16:09:57 +0100

    Day 5 part 2

--- src/main/scala/adventOfCode/Day5.scala
+++ src/main/scala/adventOfCode/Day5.scala
@@ -10,20 +10,24 @@ object Day5 extends App {
   val lines = coordinates.map {
     case xStart :: yStart :: xEnd :: yEnd :: Nil => Line(xStart, yStart, xEnd, yEnd)
   }
-  val chart = createChart(Map.empty[Point, Int], lines)
-  println(chart.count{case (_, count) => count >= 2 })
+  val chart = createChart(Map.empty[Point, Int], lines, useDiagonals = false)
+  println(chart.count { case (_, count) => count >= 2 })
+
+  val diagonalChart = createChart(Map.empty[Point, Int], lines, useDiagonals = true)
+  println(diagonalChart.count { case (_, count) => count >= 2 })
 
   @tailrec
-  def createChart(map: Map[Point, Int], lines: List[Line]): Map[Point, Int] = {
+  def createChart(map: Map[Point, Int], lines: List[Line], useDiagonals: Boolean): Map[Point, Int] = {
     lines match {
       case Nil => map
+      case line :: lines if line.isDiagonal && !useDiagonals => createChart(map, lines, useDiagonals)
       case line :: lines =>
         val updatedMap = line.getIntermediatePoints.foldLeft(map) {
           case (map, point) => map.updatedWith(point) {
             _.map(_ + 1).orElse(Some(1))
           }
         }
-        createChart(updatedMap, lines)
+        createChart(updatedMap, lines, useDiagonals)
     }
   }
 
@@ -32,9 +36,11 @@ object Day5 extends App {
     def isDiagonal: Boolean = xStart != xEnd && yStart != yEnd
 
     def getIntermediatePoints: List[Point] = {
-      if (this.isDiagonal) List.empty[Point] else {
-        val xStep = if (xStart <= xEnd) 1 else -1
-        val yStep = if (yStart <= yEnd) 1 else -1
+      val xStep = if (xStart <= xEnd) 1 else -1
+      val yStep = if (yStart <= yEnd) 1 else -1
+      if (this.isDiagonal)
+        (xStart to xEnd by xStep).zip(yStart to yEnd by yStep).map { case (x, y) => Point(x, y) }.toList
+      else {
         (for {
           x <- xStart to xEnd by xStep
           y <- yStart to yEnd by yStep
